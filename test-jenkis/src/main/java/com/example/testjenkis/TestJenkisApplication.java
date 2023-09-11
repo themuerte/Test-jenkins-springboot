30bee655604a4948e96cac6becc12243ff0066be
Eduardo Lopez <eduardorostran@yahoo.com>
2023-09-11 12:28:43 -0600

    initial project

---
+++ test-jenkis/src/main/java/com/example/testjenkis/TestJenkisApplication.java
@@ -0,0 +1,13 @@
+package com.example.testjenkis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJenkisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestJenkisApplication.class, args);
+    }
+
+}
---
+++ test-jenkis/src/test/java/com/example/testjenkis/TestJenkisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testjenkis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJenkisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
